1dd1d5ec015972bbcec39a7d6d014ef93905ad41
lab_services_student <lab_services_student@labVMH8OX>
2024-03-20 16:07:04 +0200

    Add project files.

---
+++ khumaloCraftTwo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace khumaloCraftTwo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
